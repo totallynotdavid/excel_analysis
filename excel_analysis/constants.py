c0ee5995e269b493b8c1578eaea6343c3d920ee4
David Duran <dadch1404@gmail.com>
2023-09-10 21:40:17 -0500

    style: :alien: Change the base file name

--- excel_analysis/constants.py
+++ excel_analysis/constants.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-EXCEL_FILE_NAME = 'Hoja_1.xlsx'
+EXCEL_FILE_NAME = 'PRUEBA2.xlsx'
 COLUMN_NAMES = {
     "price": "PX_LAST",
     "features": ['PX_MID', 'PX_LOW', 'PX_HIGH', 'PX_OPEN', 'RSI_14D', 'RSI_30D', 'RSI_9D', 'RSI_3D'],
