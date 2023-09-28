fb8b5d513929ccd5ae71a5db65a37dfc6152d2bb
David Duran <dadch1404@gmail.com>
2023-09-28 12:09:00 -0500

    chore: :white_check_mark: Add checks for new file

--- excel_analysis/constants.py
+++ excel_analysis/constants.py
@@ -1,11 +1,11 @@
 from collections import namedtuple
 
-EXCEL_FILE_NAME = 'PRUEBA2.xlsx'
+EXCEL_FILE_NAME = 'Prueba3.xlsx'
 COLUMN_NAMES = {
     "price": "PX_LAST",
-    "features": ['PX_MID', 'PX_LOW', 'PX_HIGH', 'PX_OPEN', 'RSI_14D', 'RSI_30D', 'RSI_9D', 'RSI_3D'],
+    "features": ['Inflacion_Mensual', 'PMI_MANUFACTURERO', 'PMI_COMPOSITE', 'ISM_MANUFACTURERO', 'INFLACION', 'TASA_INTERES', 'IPC_SINALIMENTOS_ENERGIA', 'PRODUCCIÓN_INDUSTRIAL'],
     "detail": 'DETALLE',
 }
-INDEX_COLUMN = 'FECHA'
+INDEX_COLUMN = 'Dates'
 TRAIN_TEST_SPLIT_RATIO = 0.8
 SheetResult = namedtuple("SheetResult", ["sheet_name", "final_value"])
