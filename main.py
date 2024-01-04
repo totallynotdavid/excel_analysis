1e5de109ddd3ed279675065e90a7d30ce4f2068c
David Duran <dadch1404@gmail.com>
2024-01-04 08:44:19 -0500

    Linter fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from excel_analysis import stock_processing
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Programa para procesar stock a partir de un archivo Excel
     Requiere de un archivo Excel
